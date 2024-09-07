a57d5da40bee9ec9d329d292f8f1119a23f8ffd1
vlad <vlad.bintintan@gmail.com>
2024-09-07 22:24:55 +0300

    Refactor SasiuSimplu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/SasiuSimplu.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/SasiuSimplu.java
@@ -5,23 +5,20 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.RobotHardware;
 
+/**
+ * Cel mai basic TeleOp posibil
+ */
 @TeleOp(name = "SasiuSimplu", group = "A")
-
 public class SasiuSimplu extends OpMode {
-
-        RobotHardware robot = new RobotHardware(this);
+    RobotHardware robot = new RobotHardware(this);
 
     @Override
     public void init() {
-
         robot.init();
-
     }
 
     @Override
     public void loop() {
-
         robot.movement(gamepad1);
-
     }
 }
